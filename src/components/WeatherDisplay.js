db13a43a008f364f4d42871d8632085f983c4c5a
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2024-02-11 03:05:36 +0000

    3

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -3,7 +3,7 @@ const WeatherDisplay=({mock})=>{
     const temperaturecolor= mock.temperature>20 ? "red" : "blue";
 return(
     <div>
-        <span style={{color:temperaturecolor}}>Temperature: {mock.temperature}   </span>
+        <p style={{color:temperaturecolor}}>Temperature: {mock.temperature}   </p>
         <span>Conditions: {mock.conditions}</span>
 
     </div>
